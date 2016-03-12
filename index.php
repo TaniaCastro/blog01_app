a456137b9493c9d68bdaf03766bb568e8f152a6c
TaniaCastro <tani.cas2011@gmail.com>
2016-03-12 12:37:43 -0500

    iniciando blog

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="estilos.css">
+    <title>Mi blog</title>
+</head>
+<body>
+	<div class="contenedor">
+		<nav>
+			<ul class="menu-lista">
+				<li class="menu-item"><a href="" class="menu-link">INICIO</a></li>
+				<li class="menu-item"><a href="" class="menu-link">NOSOTROS</a></li>
+				<li class="menu-item"><a href="" class="menu-link">PRODUCTOS</a></li>
+				<li class="menu-item"><a href="" class="menu-link">PORTAFOLIOS</a></li>
+				<li class="menu-item"><a href="" class="menu-link">CONTACTENOS</a></li>
+			</ul>
+		</nav>
+	<div class="slider">
+	<img src="1.jpg" alt="favorita" height="450" width="960">
+		
+		</div>
+	   <div>
+
+	   	
+	   <img src="2.jpg" alt="Mi imagen favirita" height="300" width="236">
+	   <img src="3.jpg" alt="mi imagen favorita" height="300" width="236">
+	   <img src="4.jpg" alt="mi imagen favorita" height="300" width="236">
+	   <img src="5.jpg" alt="mi imagen favorita" height="300" width="236">
+</div>
+
+
+	
+</div>
+</body>
+</html>
